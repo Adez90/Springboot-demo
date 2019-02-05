9d6e74aeaf24a32b0b2dcebef5eeea03171b02b9
WilmerKierkegaard <wilmer.kierkegaard@dynabyte.se>
2019-02-05 14:43:15 +0100

    Test 1 Wilmer

--- src/main/java/com/example/demo/controller/Testcontroller.java
+++ src/main/java/com/example/demo/controller/Testcontroller.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+/* hellooo
 
+ */
 @RestController
 public class Testcontroller {
     private static final String greeting = "Hello World!";
